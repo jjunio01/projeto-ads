5b795cc899c05ade2be489f379a3f3ee55320381
JJunio <junio.araujo01@yahoo.com>
2020-12-05 19:00:39 -0300

    Correções na classe

--- src/com/github/jjunio01/projeto/ads/entidades/Usuario.java
+++ src/com/github/jjunio01/projeto/ads/entidades/Usuario.java
@@ -6,4 +6,31 @@ package com.github.jjunio01.projeto.ads.entidades;
  */
 public class Usuario {
 
+	private String login;
+	private String senha;
+	
+	public Usuario() {
+	}
+	
+	public Usuario(String login, String senha) {
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
 }
