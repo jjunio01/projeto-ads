75246f93552ac0be880148d32aeb04ea5f7c4185
Mikael Carvalho <mcb3@discente.ifpe.edu.br>
2020-12-15 23:30:33 -0300

    Adição da class Pagamento Cartão

--- src/com/github/jjunio01/projeto/ads/pagamento/Pagamento.java
+++ src/com/github/jjunio01/projeto/ads/pagamento/Pagamento.java
@@ -1,11 +1,13 @@
 package com.github.jjunio01.projeto.ads.pagamento;
+
+import com.github.jjunio01.projeto.ads.entidades.Pessoa;
+
 /**
  * @author Mikael C. Barros
  *
  */
 public interface Pagamento {
 	
-	public boolean realizarPagamento();
-	public boolean realizarEstorno();
-	
+	public boolean realizarPagamento(double valorTotal, Pessoa cliente, EnumPag tipoPagamento, double valorPago);
+	public boolean realizarEstorno(double valorTotal, Pessoa cliente, EnumPag tipoPagamento, double valorPago);
 }
--- src/com/github/jjunio01/projeto/ads/pagamento/pagamentoCartao.java
+++ src/com/github/jjunio01/projeto/ads/pagamento/pagamentoCartao.java
@@ -1,5 +1,32 @@
 package com.github.jjunio01.projeto.ads.pagamento;
 
+import javax.swing.JOptionPane;
+
+import com.github.jjunio01.projeto.ads.entidades.Pessoa;
+
 public class pagamentoCartao {
+
+	public boolean realizarPagamento(double valorTotal, Pessoa cliente, EnumPag tipoPagamento) {
+		if (cliente.getcartaocredito().getLimite() >= valorTotal) {
+			double pagamento = cliente.getcartaocredito().getLimite() - valorTotal;
+			cliente.getcartaocredito().setLimite(pagamento);
+			JOptionPane.showMessageDialog(null, "Compra realizado com sucesso", "Status de Pagamento",
+					JOptionPane.INFORMATION_MESSAGE);
+			return true;
+		}
+
+		else {
+			return false;
+		}
+
+	}
+
+	public boolean realizarEstorno(double valorTotal, Pessoa cliente, EnumPag tipoPagamento) {
+		double estorno = cliente.getcartaocredito().getLimite() + valorTotal;
+		cliente.getcartaocredito().setLimite(estorno);
+		JOptionPane.showMessageDialog(null, "Estorno realizado com sucesso", "Status do estorno",
+				JOptionPane.INFORMATION_MESSAGE);
+		return true;
+	}
 
 }
