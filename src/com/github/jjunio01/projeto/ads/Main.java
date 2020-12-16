07ea8359459359446d365afb559c61f36110e1e1
JJunio <junio.araujo01@yahoo.com>
2020-12-15 22:02:45 -0300

    Criação de métodos na classe Man para execução do Projeto

--- src/com/github/jjunio01/projeto/ads/Main.java
+++ src/com/github/jjunio01/projeto/ads/Main.java
@@ -1,5 +1,11 @@
 package com.github.jjunio01.projeto.ads;
 
+import java.util.List;
+
+import com.github.jjunio01.projeto.ads.entidades.Pessoa;
+import com.github.jjunio01.projeto.ads.entidades.Usuario;
+import com.github.jjunio01.projeto.ads.estoque.Estoque;
+import com.github.jjunio01.projeto.ads.estoque.Produto;
 
 /**
  * @author JJunio
@@ -8,9 +14,59 @@ package com.github.jjunio01.projeto.ads;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
 
+	}
+
+	public static void cadastrarPessoa() {
+
+	}
+
+	public static void cadastrarUsuario() {
+
+	}
+
+	public static void cadastrarProduto() {
+
+	}
+
+	public static Estoque cadastrarEstoque() {
+		return null;
+	}
+
+	public static Pessoa recuperarPessoa(String nome) {
+		return null;
+	}
+
+	public static Usuario recuperarUsuario(String login) {
+		return null;
+	}
+
+	public static Produto recuperarProduto(String nome) {
+		return null;
+	}
+	
+	public static Produto recuperarProduto(int id) {
+		return null;
+	}
+
+	public static Estoque recuperarEstoque(Produto produto) {
+		return null;
+	}
+	
+	public static List<Pessoa> recuperarTodasPessoas() {
+		return null;
+	}
+
+	public static List<Usuario> recuperarTodosUsuarios() {
+		return null;
+	}
+
+	public static List<Produto> recuperarTodosProdutos() {
+		return null;
+	}
+
+	public static List<Estoque> recuperarTodoEstoque() {
+		return null;
 	}
 
 }
