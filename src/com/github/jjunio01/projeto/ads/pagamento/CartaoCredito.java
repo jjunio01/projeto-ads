0c52c4ea3fe7e80293907954852d856f2a497188
Mikael Carvalho <mcb3@discente.ifpe.edu.br>
2020-12-19 17:06:18 -0300

    Alteração em Cartão Credito

--- src/com/github/jjunio01/projeto/ads/pagamento/CartaoCredito.java
+++ src/com/github/jjunio01/projeto/ads/pagamento/CartaoCredito.java
@@ -6,17 +6,18 @@ import java.io.Serializable;
  * @author Mikael C. Barros
  *
  */
-public class CartaoCredito implements Serializable  {
-	
+public class CartaoCredito implements Serializable {
+
 	private EnumBandeira bandeira;
 	private String numero;
 	private double limite;
-	private int cvv;
+	private String cvv;
 	private String nome;
-	private int validade;
-	
-	
-	public CartaoCredito(EnumBandeira bandeira, String numero, double limite, int cvv, String nome, int validade) {
+	private String validade;
+
+	public CartaoCredito(EnumBandeira bandeira, String numero, double limite, String cvv, String nome,
+			String validade) {
+
 		this.bandeira = bandeira;
 		this.numero = numero;
 		this.limite = limite;
@@ -24,66 +25,57 @@ public class CartaoCredito implements Serializable  {
 		this.nome = nome;
 		this.validade = validade;
 	}
-
 
 	public EnumBandeira getBandeira() {
 		return bandeira;
 	}
 
-
 	public void setBandeira(EnumBandeira bandeira) {
 		this.bandeira = bandeira;
 	}
-
 
 	public String getNumero() {
 		return numero;
 	}
 
-
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
-
 
 	public double getLimite() {
 		return limite;
 	}
 
-
 	public void setLimite(double limite) {
 		this.limite = limite;
 	}
 
-
-	public int getCvv() {
+	public String getCvv() {
 		return cvv;
 	}
 
-
-	public void setCvv(int cvv) {
+	public void setCvv(String cvv) {
 		this.cvv = cvv;
 	}
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
-	public int getValidade() {
+	public String getValidade() {
 		return validade;
 	}
 
-
-	public void setValidade(int validade) {
+	public void setValidade(String validade) {
 		this.validade = validade;
 	}
 
-
+	
+	
+	
+	
 }
